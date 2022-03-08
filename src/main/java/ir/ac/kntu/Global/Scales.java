bc3c1b767154dc331df8d7c529234df4966169a7
Alireza Sadeghi <alireza_sad@yahoo.com>
2022-03-08 17:44:22 +0330

    scales that use for the entire app

---
+++ src/main/java/ir/ac/kntu/Global/Scales.java
@@ -0,0 +1,54 @@
+package ir.ac.kntu.Global;
+
+/**
+ * @author Alireza Sadeghi
+ * store all scales that need for controllers
+ */
+public class Scales {
+    public static final int PANE_HEIGHT = 500;
+    public static final int PANE_WIDTH = 750;
+    public static final int DC_COMBO_X = 275;
+    public static final int DC_COMBO_Y = 50;
+    public static final int DC_COMBO_HEIGHT = 30;
+    public static final int DC_COMBO_WIDTH = 200;
+    public static final int TITLE_X = 275;
+    public static final int TITLE_Y = 130;
+    public static final int TITLE_HEIGHT = 30;
+    public static final int TITLE_WIDTH = 200;
+    public static final int AUTHOR_X = 275;
+    public static final int AUTHOR_Y = 210;
+    public static final int AUTHOR_HEIGHT = 30;
+    public static final int AUTHOR_WIDTH = 200;
+    public static final int DATE_X = 275;
+    public static final int DATE_Y = 290;
+    public static final int DATE_HEIGHT = 30;
+    public static final int DATE_WIDTH = 200;
+    public static final int UNI_X = 275;
+    public static final int UNI_Y = 370;
+    public static final int UNI_HEIGHT = 30;
+    public static final int UNI_WIDTH = 200;
+    public static final int SUBMIT_X = 325;
+    public static final int SUBMIT_Y = 430;
+    public static final int SUBMIT_HEIGHT = 30;
+    public static final int SUBMIT_WIDTH = 100;
+    public static final int POP_UP_HEIGHT = 250;
+    public static final int POP_UP_WIDTH = 400;
+    public static final int POP_UP_TXT_X = 100;
+    public static final int POP_UP_TXT_Y = 50;
+    public static final int POP_UP_TXT_HEIGHT = 30;
+    public static final int POP_UP_TXT_WIDTH = 210;
+    public static final int ADD_SEC_BUTTON_X = 50;
+    public static final int ADD_SEC_BUTTON_Y = 80;
+    public static final int ADD_SEC_BUTTON_HEIGHT = 25;
+    public static final int ADD_SEC_BUTTON_WIDTH = 25;
+    public static final int SEC_TITLE_X = 50;
+    public static final int SEC_TITLE_Y = 160;
+    public static final int SEC_TITLE_HEIGHT = 30;
+    public static final int SEC_TITLE_WIDTH = 200;
+    public static final int SEC_PAR_X = 50;
+    public static final int SEC_PAR_Y = 230;
+    public static final int SEC_PAR_HEIGHT = 200;
+    public static final int SEC_PAR_WIDTH = 650;
+    public static final int PANE_TITLE_X = PANE_WIDTH/2;
+    public static final int PANE_TITLE_Y = PANE_HEIGHT/8;
+}
